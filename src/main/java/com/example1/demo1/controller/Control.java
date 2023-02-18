61d697552e4926465d41661de8eb0f7852db8a12
nguyen minh hoa <nguyenhoa28598@gmail.com>
2023-02-18 22:54:39 +0900

    0218

--- src/main/java/com/example1/demo1/controller/Control.java
+++ src/main/java/com/example1/demo1/controller/Control.java
@@ -6,10 +6,7 @@ import com.example1.demo1.repositori.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -35,7 +32,11 @@ public class Control {
                     new ResponseObject("false","Cannot find product with id = "+id,""));
         }
     }
-//    Optional<modeProduct> findById(@PathVariable Long id) {
-//        return repository.findById(id);
-//    }
+    @PostMapping("/insert")
+    ResponseEntity<ResponseObject> insertProduct(@RequestBody modeProduct newpProduct){
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("ok","Insert Product Successfully",repository.save(newpProduct))
+        );
+    }
+
 }
